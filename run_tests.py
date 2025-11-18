16a246487ac48d4beb7ae4566bcc695bcb1211a4
OppositeNor <oppositenormal@outlook.com>
2025-11-18 15:08:50 -0500

    set error code for valgrind

--- run_tests.py
+++ run_tests.py
@@ -2,6 +2,6 @@ import os
 import subprocess
 from build_setup import build_dir
 
-run_script = ["valgrind", str(os.path.join(build_dir, 'tests/wbe_unit_test'))]
+run_script = ["valgrind", "--error-exitcode=42", str(os.path.join(build_dir, 'tests/wbe_unit_test'))]
 
 subprocess.run(run_script)
